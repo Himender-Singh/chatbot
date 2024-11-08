68f28d160fe05abe6c200a23553b19b5c3809232
Himanshu <himanshutawer34@gmail.com>
2024-11-08 16:36:07 +0530

    new changes

--- backend/database/db.js
+++ backend/database/db.js
@@ -2,55 +2,15 @@ import mongoose from "mongoose";
 
 const connectDb = async () => {
   try {
-    await mongoose.connect(process.env.Db_url, {
+    mongoose.set('strictQuery', false); // Disable strict query mode
+    await mongoose.connect(process.env.MONGODB_URI, {
       dbName: "Chatbot",
     });
 
-    console.log("Mongo db connected");
+    console.log("MongoDB connected");
   } catch (error) {
     console.log(error);
   }
 };
 
 export default connectDb;
-
-// import express from "express";
-// import dotenv from "dotenv";
-// import connectDb from "./database/db.js";
-// import cors from "cors";
-// import path from "path";
-// // importing routes
-// import userRoutes from "./routes/userRoutes.js";
-// import chatRoutes from "./routes/chatRoutes.js";
-
-// dotenv.config();
-
-// const app = express();
-
-// // using middleware
-// app.use(express.json());
-
-// // CORS configuration to allow both domains
-// const corsOptions = {
-//   origin: 'http://localhost:5173', // Add the allowed origins
-//   credentials: true
-// };
-// app.use(cors(corsOptions));
-
-// const __dirname = path.resolve();
-
-// // using routes
-// app.use("/api/user", userRoutes);
-// app.use("/api/chat", chatRoutes);
-
-// // Serve frontend assets
-// app.use(express.static(path.join(__dirname, "/frontend/dist")));
-// app.get("*", (req, res) => {
-//   res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-// });
-
-// // Start the server and connect to the database
-// app.listen(process.env.PORT, () => {
-//   console.log(`Server is working on port ${process.env.PORT}`);
-//   connectDb();
-// });
