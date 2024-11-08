248b787c3205d0ae4166c608c0702513bed26379
Himanshu <himanshutawer34@gmail.com>
2024-11-08 21:56:33 +0530

    url changes

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const app = express();
 
 // CORS options
 const corsOptions = {
-  origin: "http://localhost:5173",  // Specify allowed origin
+  origin: "https://chatbot-last-bf47.onrender.com",  // Specify allowed origin
   methods: "GET,POST,PUT,DELETE",                // Allowed HTTP methods
   allowedHeaders: "Content-Type,Authorization",   // Allowed headers
 };
