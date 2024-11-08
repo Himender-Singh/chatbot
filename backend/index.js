10fa3e1809f86a8129af9045dbd438ec6c312f44
Himanshu <himanshutawer34@gmail.com>
2024-11-08 17:14:43 +0530

    url change

--- backend/index.js
+++ backend/index.js
@@ -11,9 +11,17 @@ dotenv.config();
 
 const app = express();
 
+// CORS options
+const corsOptions = {
+  origin: "https://chatbot-career.onrender.com",  // Specify allowed origin
+  methods: "GET,POST,PUT,DELETE",                // Allowed HTTP methods
+  allowedHeaders: "Content-Type,Authorization",   // Allowed headers
+};
+
 // using middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));  // Apply the corsOptions here
+
 const __dirname = path.resolve();
 
 // using routes
